f0138b70ac0c0371160fe1d969685cd440929b4d
Smirnova Marina <marinaMoscow2003@list.ru>
2018-10-31 21:05:27 +0300

    Домашнее задание 1

--- src/Resume.java
+++ src/Resume.java
@@ -6,8 +6,27 @@ public class Resume {
     // Unique identifier
     String uuid;
 
+    public Resume() {
+    }
+
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
     @Override
     public String toString() {
         return uuid;
+    }
+
+    public boolean equals(Resume obj) {
+        return this.uuid.equals(obj.uuid);
     }
 }
